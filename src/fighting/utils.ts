45c297bf48275ed3abbd37f39aefa55d8606b960
Ran-Regev-HLS <ranr@moveohls.com>
2024-12-02 17:08:31 +0200

    added data advantage

--- src/fighting/utils.ts
+++ src/fighting/utils.ts
@@ -34,11 +34,13 @@ export function calculateAttack(
       0,
     );
 
-  const finalDamage = Math.max(0, rawDamage * randomFactor);
+    const typeMultiplier = getTypeEffectivenessMultiplier(attacker.type, defender.type);
 
-  const newDefenderHP = Math.max(0, defenderHP - finalDamage);
+    const finalDamage = Math.max(0, rawDamage * randomFactor * typeMultiplier);
 
-  return newDefenderHP;
+    const newDefenderHP = Math.max(0, defenderHP - finalDamage);
+
+    return newDefenderHP;
 }
 
 function didAttackMiss(
@@ -87,6 +89,41 @@ export function attemptCatch(opponentHp: number, BaseHP: number): boolean {
   const randomValue = Math.random();
   const catchRate = calculateCatchRate(opponentHp, BaseHP);
   return randomValue <= catchRate;
+}
+
+const TYPE_EFFECTIVENESS: Record<string, Record<string, number>> = {
+    Normal: { Rock: 0.5, Ghost: 0, Steel: 0.5 },
+    Fire: { Grass: 2, Ice: 2, Bug: 2, Steel: 2, Fire: 0.5, Water: 0.5, Rock: 0.5, Dragon: 0.5 },
+    Water: { Fire: 2, Ground: 2, Rock: 2, Water: 0.5, Grass: 0.5, Dragon: 0.5 },
+    Electric: { Water: 2, Flying: 2, Electric: 0.5, Grass: 0.5, Ground: 0, Dragon: 0.5 },
+    Grass: { Water: 2, Ground: 2, Rock: 2, Fire: 0.5, Grass: 0.5, Poison: 0.5, Flying: 0.5, Bug: 0.5, Dragon: 0.5, Steel: 0.5 },
+    Ice: { Grass: 2, Ground: 2, Flying: 2, Dragon: 2, Fire: 0.5, Water: 0.5, Ice: 0.5, Steel: 0.5 },
+    Fighting: { Normal: 2, Ice: 2, Rock: 2, Dark: 2, Steel: 2, Poison: 0.5, Flying: 0.5, Psychic: 0.5, Bug: 0.5, Fairy: 0.5, Ghost: 0 },
+    Poison: { Grass: 2, Fairy: 2, Poison: 0.5, Ground: 0.5, Rock: 0.5, Ghost: 0.5, Steel: 0 },
+    Ground: { Fire: 2, Electric: 2, Poison: 2, Rock: 2, Steel: 2, Grass: 0.5, Bug: 0.5, Flying: 0 },
+    Flying: { Grass: 2, Fighting: 2, Bug: 2, Electric: 0.5, Rock: 0.5, Steel: 0.5 },
+    Psychic: { Fighting: 2, Poison: 2, Psychic: 0.5, Steel: 0.5, Dark: 0 },
+    Bug: { Grass: 2, Psychic: 2, Dark: 2, Fire: 0.5, Fighting: 0.5, Poison: 0.5, Flying: 0.5, Ghost: 0.5, Steel: 0.5, Fairy: 0.5 },
+    Rock: { Fire: 2, Ice: 2, Flying: 2, Bug: 2, Fighting: 0.5, Ground: 0.5, Steel: 0.5 },
+    Ghost: { Psychic: 2, Ghost: 2, Normal: 0, Dark: 0.5 },
+    Dragon: { Dragon: 2, Steel: 0.5, Fairy: 0 },
+    Dark: { Psychic: 2, Ghost: 2, Fighting: 0.5, Dark: 0.5, Fairy: 0.5 },
+    Steel: { Ice: 2, Rock: 2, Fairy: 2, Fire: 0.5, Water: 0.5, Electric: 0.5, Steel: 0.5 },
+    Fairy: { Fighting: 2, Dragon: 2, Dark: 2, Fire: 0.5, Poison: 0.5, Steel: 0.5 }
+};
+
+
+function getTypeEffectivenessMultiplier(
+    attackerTypes: string[],
+    defenderTypes: string[]
+): number {
+    let multiplier = 1;
+    attackerTypes.forEach(attackerType => {
+        defenderTypes.forEach(defenderType => {
+            multiplier *= TYPE_EFFECTIVENESS[attackerType]?.[defenderType] || 1;
+        });
+    });
+    return multiplier;
 }
 
 export function pokemonUneededData(field: string) {
